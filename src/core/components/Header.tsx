4deaf8b5931233755a5082fe8a7e49d2d0170a89
Matheus Botelho <matheusbcdev@gmail.com>
2023-05-28 21:43:39 -0300

    feat: go back button in header

--- src/core/components/Header.tsx
+++ src/core/components/Header.tsx
@@ -1,29 +1,35 @@
-import {StyleSheet} from 'react-native';
+import {View} from 'react-native';
 
 import {NativeStackHeaderProps} from '@react-navigation/native-stack';
-
-import {FontAwesome5} from '@expo/vector-icons';
+import {FontAwesome5, FontAwesome} from '@expo/vector-icons';
 
 import {Row, Button} from '../../shared/components';
 import {useSafeAreaInsets} from '../../shared/hooks';
 
-const styles = StyleSheet.create({
-  container: {
-    justifyContent: 'flex-end',
-  },
-});
-
-export function Header({navigation}: NativeStackHeaderProps) {
+export function Header({navigation, route, back}: NativeStackHeaderProps) {
   const {top} = useSafeAreaInsets();
 
   return (
-    <Row style={[{paddingTop: top, height: 50 + top}, styles.container]}>
-      <Button
-        onPress={() => navigation.navigate('Cart')}
-        padding="xs"
-        marginHorizontal="m">
-        <FontAwesome5 name="shopping-cart" size={24} color="black" />
-      </Button>
+    <Row style={[{paddingTop: top, height: 50 + top}]}>
+      {back && (
+        <Button
+          onPress={() => navigation.pop()}
+          padding="xs"
+          marginHorizontal="m">
+          <FontAwesome name="arrow-left" size={24} color="black" />
+        </Button>
+      )}
+
+      <View style={{flex: 1}} />
+
+      {route.name !== 'Cart' && (
+        <Button
+          onPress={() => navigation.navigate('Cart')}
+          padding="xs"
+          marginHorizontal="m">
+          <FontAwesome5 name="shopping-cart" size={24} color="black" />
+        </Button>
+      )}
     </Row>
   );
 }
